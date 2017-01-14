1b133ae3c9918226540bd2668ceebb3a1af15c19
GRZeng <zenggr@gzsendi.cn>
2017-01-14 21:45:26 +0800

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+    devtool: 'eval',
+    entry: [
+        'webpack-dev-server/client?http://localhost:3000',
+        'webpack/hot/only-dev-server',
+        './src/index'
+    ],
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    module: {
+        loaders: [
+
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+               
+            }, {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.join(__dirname, 'src')
+            }, {
+                test: /\.(gif|jpg|png|woff|woff2|svg|eot|ttf)\??.*$/,
+                loader: 'url-loader?limit=50000&name=[path][name].[ext]'
+            }
+
+        ]
+    }
+};
